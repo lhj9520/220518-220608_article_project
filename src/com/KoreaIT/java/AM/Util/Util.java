e233345de5f4924819e7ef6cd2ad772290405f43
lhj9520 <gw9520@naver.com>
2022-05-25 11:48:53 +0900

    게시판 리팩토링

--- src/com/KoreaIT/java/AM/Util/Util.java
+++ src/com/KoreaIT/java/AM/Util/Util.java
@@ -1,7 +1,9 @@
+package com.KoreaIT.java.AM.Util;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Util_DateTime {
+public class Util {
 	public static String getNowDateTimeStr() {
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		Date time = new Date();
---
+++ src/com/KoreaIT/java/AM/dto/Article.java
@@ -0,0 +1,26 @@
+package com.KoreaIT.java.AM.dto;
+
+public class Article{
+	public int id;
+	public String title;
+	public String content;
+	public String datetime;
+	public int hit;
+	
+	//메서드 이름은 같고 매개변수의 개수가 다르면 다른 함수로 인식
+	public Article(int id, String title, String body, String datetime){
+		this(id,title,body,datetime,0); //다른 메서드 실행
+	}
+	
+	public Article(int id, String title, String body, String datetime, int hit){
+		this.id = id;
+		this.title = title;
+		this.content = body;
+		this.datetime = datetime;
+		this.hit = hit;
+	}
+	
+	public void increaseHit() {
+		hit++;
+	}
+}
