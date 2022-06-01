62e6b8825e33caca9648ee944cce73f7c7e2656c
lhj9520 <gw9520@naver.com>
2022-06-01 10:55:24 +0900

    로그인세션 수정

--- src/com/KoreaIT/java/AM/controller/MemberController.java
+++ src/com/KoreaIT/java/AM/controller/MemberController.java
@@ -31,7 +31,7 @@ public class MemberController extends Controller {
 			doLogin();
 			break;
 		case "whoami":
-			checkLoginstatus();
+			checkWhoami();
 			break;
 		case "logout":
 			doLogout();
@@ -45,7 +45,7 @@ public class MemberController extends Controller {
 	public void makeTestData() {
 
 		System.out.println("테스트를 위한 member 데이터를 생성합니다.");
-
+		member.add(new Member(1, Util.getNowDateTimeStr(), "admin", "admin", "관리자"));
 		member.add(new Member(1, Util.getNowDateTimeStr(), "1", "1", "1"));
 		member.add(new Member(2, Util.getNowDateTimeStr(), "2", "2", "2"));
 		member.add(new Member(3, Util.getNowDateTimeStr(), "3", "3", "3"));
@@ -55,46 +55,45 @@ public class MemberController extends Controller {
 		int idx = member.size() + 1;
 		String userid = null;
 
-		if (loginedmember == null) {
-			while (true) {
-				System.out.printf("아이디를 입력하세요 : ");
-				userid = sc.nextLine();
+		if (isLogined()) {
+			System.out.printf("%s님 로그인 상태입니다.\n", loginedmember.mid);
+			return;
+		}
+		while (true) {
+			System.out.printf("아이디를 입력하세요 : ");
+			userid = sc.nextLine();
 
-				if (id_Duplicatecheck(userid)) { // 중복 없음
-					break;
-				}
-				System.out.printf("%s는(은) 중복된 ID입니다.\n", userid);
-			}
-
-			String userpwd = null;
-			String userpwdconfirm = null;
-
-			while (true) {
-				// PWD 입력 받기
-				System.out.printf("비밀번호를 입력하세요 : ");
-				userpwd = sc.nextLine();
-				System.out.printf("비밀번호 확인: ");
-				userpwdconfirm = sc.nextLine();
-
-				if (!userpwd.equals(userpwdconfirm)) {
-					System.out.println("비밀번호를 다시 입력하세요.");
-					continue;
-				}
+			if (id_Duplicatecheck(userid)) { // 중복 없음
 				break;
 			}
-
-			// 이름 입력 받기
-			System.out.printf("이름을 입력하세요 : ");
-			String name = sc.nextLine();
-
-			Member members = new Member(idx, Util.getNowDateTimeStr(), userid, userpwd, name);
-			member.add(members);
-
-			System.out.println(idx + "번 회원가입이 완료되었습니다.");
-		} else {
-			System.out.printf("%s님 로그인 상태입니다.\n", loginedmember.mid);
+			System.out.printf("%s는(은) 중복된 ID입니다.\n", userid);
 		}
 
+		String userpwd = null;
+		String userpwdconfirm = null;
+
+		while (true) {
+			// PWD 입력 받기
+			System.out.printf("비밀번호를 입력하세요 : ");
+			userpwd = sc.nextLine();
+			System.out.printf("비밀번호 확인: ");
+			userpwdconfirm = sc.nextLine();
+
+			if (!userpwd.equals(userpwdconfirm)) {
+				System.out.println("비밀번호를 다시 입력하세요.");
+				continue;
+			}
+			break;
+		}
+
+		// 이름 입력 받기
+		System.out.printf("이름을 입력하세요 : ");
+		String name = sc.nextLine();
+
+		Member members = new Member(idx, Util.getNowDateTimeStr(), userid, userpwd, name);
+		member.add(members);
+
+		System.out.println(idx + "번 회원가입이 완료되었습니다.");
 	}
 
 	private void doLogin() {
@@ -102,50 +101,57 @@ public class MemberController extends Controller {
 		String inputpw = null;
 		Member membercheck = null;
 
-		if (loginedmember == null) {
-			System.out.printf("아이디를 입력하세요 : ");
-			inputid = sc.nextLine();
-
-			System.out.printf("비밀번호를 입력하세요 : ");
-			inputpw = sc.nextLine();
-
-			membercheck = getMemberByLoginId(inputid);
-
-			if (membercheck == null) {
-				System.out.println("아이디가 존재하지 않습니다.\n");
-				return;
-			}
-
-			if (inputid.equals(membercheck.mid) && inputpw.equals(membercheck.mpwd)) {
-				System.out.printf("%s님 로그인에 성공하였습니다.\n", inputid);
-				loginedmember = membercheck;
-			} else {
-				System.out.println("아이디 혹은 비밀번호가 일치하지 않습니다.");
-			}
-		} else {
+		if (isLogined()) {
 			System.out.printf("%s님 로그인 상태입니다.\n", loginedmember.mid);
+			return;
+		}
+
+		System.out.printf("아이디를 입력하세요 : ");
+		inputid = sc.nextLine();
+
+		System.out.printf("비밀번호를 입력하세요 : ");
+		inputpw = sc.nextLine();
+
+		membercheck = getMemberByLoginId(inputid);
+
+		if (membercheck == null) {
+			System.out.println("아이디가 존재하지 않습니다.\n");
+			return;
+		}
+
+		if (inputid.equals(membercheck.mid) && inputpw.equals(membercheck.mpwd)) {
+			System.out.printf("로그인 성공! %s님 환영합니다.\n", membercheck.mname);
+			loginedmember = membercheck;
+		} else {
+			System.out.println("아이디 혹은 비밀번호가 일치하지 않습니다.");
 		}
 
 	}
 
-	private void checkLoginstatus() {
-		if (loginedmember == null) {
-			System.out.println("로그아웃 상태입니다.");
-		} else {
-			System.out.println("=========================");
-			System.out.printf("id : %s\n", loginedmember.mid);
-			System.out.printf("password : %s\n", loginedmember.mpwd);
-			System.out.println("=========================");
+	private void checkWhoami() {
+		if (!isLogined()) {
+			System.out.println("로그인 상태가 아닙니다.");
+			return;
 		}
+		System.out.println("=========================");
+		System.out.printf("id : %s\n", loginedmember.mid);
+		System.out.printf("password : %s\n", loginedmember.mpwd);
+		System.out.printf("이름 : %s\n", loginedmember.mname);
+		System.out.println("=========================");
 	}
 
 	private void doLogout() {
-		if (loginedmember == null) {
-			System.out.println("로그아웃 상태입니다.");
-		} else {
-			System.out.printf("%s님 로그아웃 되었습니다.\n", loginedmember.mid);
-			loginedmember = null;
+		if (!isLogined()) {
+			System.out.println("로그인 상태가 아닙니다.");
+			return;
 		}
+		System.out.printf("%s님 로그아웃 되었습니다.\n", loginedmember.mid);
+		loginedmember = null;
+	}
+
+	// 로그인 상태 확인
+	private boolean isLogined() {
+		return loginedmember != null;
 	}
 
 	// 아이디 체크
